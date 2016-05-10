60fd5d326cf3c1e0636adfa041957deed36c2e5a
mac785 <jamie.moseley@kinkaid.org>
2016-05-10 14:22:13 -0500

    Begin of Class May 10

---
+++ LedgeFile.py
@@ -0,0 +1,60 @@
+__author__ = 'Jimmyjamz'  # Update this!!!
+import pygame
+import random
+import Constants
+
+
+class Ledge:  # fix this... and then delete this comment.
+
+    def __init__(self):
+        """
+        This automatically gets called when you first create this object.
+        """
+        self.x = 200
+        self.y = 300
+        self.vx = 0
+        self.vy = 0
+        self.iAmAlive = True
+        self.width = 70
+        self.height = Constants.PLATFORM_HEIGHT
+
+    def isDead(self):
+        """
+        this is a method used by the main loop to determine whether it
+        is time to remove this object from the screen and the list of
+        objects.
+        :return: whether this object is dead now.
+        """
+        if self.iAmAlive:
+            return False
+        else:
+            return True
+            # alternative (1-line) version of this function:
+            #  "return not self.iAmAlive"
+
+    def drawSelf(self, surface):
+        """
+        this is called by the main loop and tells us to draw
+        whatever this shape looks like onto the given surface.
+        :param surface: what to draw on.
+        """
+        color = pygame.Color(128, 64, 0)
+        pygame.draw.rect(surface, color, (self.x - self.width / 2, self.y - self.height / 2, self.width, self.height))
+
+    def step(self, deltaT):
+        """
+        this is called by the main loop and tells us to advance
+        this object by one animation step - perhaps to move this
+        object or change its appearance.
+        :param deltaT: how long it has been since the last step.
+        """
+        pass
+
+    def die(self):
+        """
+        This is how another object (or the main game) can tell
+        this one it is time to go away. (Often used after a
+         collision.)
+        :return:
+        """
+        self.iAmAlive = False
--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
-import pygame, sys, traceback, JumpMan, PlatformFile, Constants
+import pygame, sys, traceback, JumpMan, PlatformFile, Constants, LedgeFile
 from pygame.locals import *
 
-__author__ = 'Jimmyjamz'  #put your name here!!!
+__author__ = 'Jimmyjamz'  # put your name here!!!
+
+
 # =====================  pre-setup()
 def preSetup():
     """
@@ -10,14 +12,15 @@ def preSetup():
     You shouldn't change this method (much) - you can play with screensize & font.
     """
     global buffer, debugFont, objectsOnScreen
-    buffer = pygame.display.set_mode((400,400),pygame.DOUBLEBUF) #window size
-    debugFont = pygame.font.SysFont("Helvetica", 12) #this is the font for
-                                                    #the bottom of the screen.
-    buffer.fill((128,0,255))
+    buffer = pygame.display.set_mode((400, 400), pygame.DOUBLEBUF)  # window size
+    debugFont = pygame.font.SysFont("Helvetica", 12)  # this is the font for
+    # the bottom of the screen.
+    buffer.fill((128, 0, 255))
     pygame.display.flip()
-    objectsOnScreen = [] #this is a list of all things that
-                        # should be drawn on screen. It starts
-                        # off empty, but we can add stuff to it.
+    objectsOnScreen = []  # this is a list of all things that
+    # should be drawn on screen. It starts
+    # off empty, but we can add stuff to it.
+
 
 # =====================  setup()
 def setup():
@@ -26,8 +29,8 @@ def setup():
     Unlike the "preSetup()" method, you are welcome to change this!
     """
 
-    global jumpMan, leftKeyPressed, rightKeyPressed, upKeyPressed, downKeyPressed, jumpReady
-    global barrelList, platformList
+    global jumpMan, leftKeyPressed, rightKeyPressed, upKeyPressed, downKeyPressed
+    global barrelList, platformList, ledgeList
 
     jumpMan = JumpMan.JumpMan()
     objectsOnScreen.append(jumpMan)
@@ -37,18 +40,27 @@ def setup():
     upKeyPressed = False
     downKeyPressed = False
 
-    jumpReady = False
+    ledgeList = []
+    ledge1 = LedgeFile.Ledge()
+    objectsOnScreen.append(ledge1)
+    ledgeList.append(ledge1)
+    ledge1.x = 200
+    ledge1.y = 340
 
     platformList = []
     platform1 = PlatformFile.Platform()
     objectsOnScreen.append(platform1)
     platformList.append(platform1)
-    platform1.x = 300
+    platform1.x = 320
+    platform1.y = 320
 
     platform2 = PlatformFile.Platform()
     objectsOnScreen.append(platform2)
     platformList.append(platform2)
-    platform2.x = 100
+    platform2.x = 200
+    platform2.y = 390
+    platform2.width = 400
+
 
 # =====================  loop()
 def loop(deltaT):
@@ -64,18 +76,21 @@ def loop(deltaT):
     clearDeadObjects()
     drawObjects()
 
-    debugDisplay(buffer,deltaT) #this makes the text at the bottom of the
-                                #screen. Feel free to comment this line out.
-    pygame.display.flip() # updates the window to show the latest
-                          # version of the buffer.
+    debugDisplay(buffer, deltaT)  # this makes the text at the bottom of the
+    # screen. Feel free to comment this line out.
+    pygame.display.flip()  # updates the window to show the latest
+    # version of the buffer.
+
 
 # =====================  drawBackground()
 def drawBackground():
-    buffer.fill([0,0,128]) #clear screen with background color.
-                           # (feel free to pick a different color.)
-                           # (red, green, blue) 0-255.
+    buffer.fill([0, 0, 128])  # clear screen with background color.
+    # (feel free to pick a different color.)
+    # (red, green, blue) 0-255.
 
-    #alternately, you might later decide to blit in a background graphic.
+    # alternately, you might later decide to blit in a background graphic.
+
+
 # =====================  animateObjects()
 def animateObjects(deltaT):
     """
@@ -87,14 +102,15 @@ def animateObjects(deltaT):
     if rightKeyPressed and not leftKeyPressed:
         jumpMan.moveRight()
     if upKeyPressed:
-        jumpMan.y -= 2
-        #jumpMan.jump()
+        jumpMan.jump()
     if downKeyPressed:
         jumpMan.y += 2
     for object in objectsOnScreen:
         if object.isDead():
-            continue # skip the dead ones....
+            continue  # skip the dead ones....
         object.step(deltaT)
+
+
 # =====================  checkForInteractions()
 def checkForInteractions():
     """
@@ -102,12 +118,10 @@ def checkForInteractions():
     one another.
     """
     checkJumpManPlatformCollisions()
-    if jumpMan.status == Constants.STATUS_WALKING:
-        jumpReady = True
-    else:
-        jumpReady = False
-    #checkJumpManBarrelCollisions()
-    #checkJumpManLadderClimb()
+    # checkJumpManBarrelCollisions()
+    # checkJumpManLadderClimb()
+
+
 # =====================  clearDeadObjects()
 def clearDeadObjects():
     """
@@ -130,6 +144,7 @@ def drawObjects():
     for object in objectsOnScreen:
         object.drawSelf(buffer)
 
+
 # =====================  debugDisplay()
 def debugDisplay(buffer, deltaT):
     """
@@ -141,10 +156,70 @@ def debugDisplay(buffer, deltaT):
     """
     surface_rect = buffer.get_rect()
     objectCountSurface = debugFont.render("Objects: {0}".format(len(objectsOnScreen)), True, (255, 255, 255))
-    buffer.blit(objectCountSurface,(3,surface_rect.h-15))
+    buffer.blit(objectCountSurface, (3, surface_rect.h - 15))
 
     fpsSurface = debugFont.render("{0:3.1f} FPS".format(1 / deltaT), True, (255, 255, 255))
-    buffer.blit(fpsSurface, ((surface_rect.w-5) - debugFont.size("{0:3.1f} FPS".format(1 / deltaT))[0], (surface_rect.h-15)))
+    buffer.blit(fpsSurface, ((surface_rect.w - 5) - debugFont.size("{0:3.1f} FPS".format(1 / deltaT))[0], (surface_rect.h - 15)))
+
+
+def checkJumpManLedgeCollisions():
+    global jumpMan
+    if jumpMan.status == Constants.STATUS_JUMPING:
+        for l in ledgeList:
+            # Check whether we hit the right edge of the platform....
+            # 1) are we Jumping?
+            # 2) Jumping to the left?
+            # 3) Overlapping right edge of platform in x?
+            # 4) Within 10 pixels of right edge of platform in x?
+            # 5) Overlapping with bottom edge of platform?
+            # 6) Overlapping with top edge of platform?
+
+            if jumpMan.status == Constants.STATUS_JUMPING and jumpMan.vx < 0 and jumpMan.x - l.x < jumpMan.width / 2 + l.width / 2 and jumpMan.x - jumpMan.width / 2 > l.x + l.width / 2 - 10 and jumpMan.y - l.y < jumpMan.height / 2 + l.height / 2 and l.y - jumpMan.y < jumpMan.height / 2 + l.height:
+
+                jumpMan.vx *= -1
+                # find out how much the ostrich's left edge is past the platform's right edge
+                overlap = (l.x + l.width / 2) - (jumpMan.x - jumpMan.width / 2)
+                # "bounce" the ostrich away from the right edge, by that amount.
+                jumpMan.x = (l.x + l.width / 2) + jumpMan.width / 2 + overlap
+
+
+            # Check whether we hit the left edge of the platform....
+            # 1) are we Jumping?
+            # 2) Jumping to the right?
+            # 3) Overlapping left edge of platform in x?
+            # 4) Within 10 pixels of left edge of platform in x?
+            # 5) Overlapping with bottom edge of platform?
+            # 6) Overlapping with top edge of platform?
+
+            elif jumpMan.status == Constants.STATUS_JUMPING and jumpMan.vx > 0 and l.x - jumpMan.x < jumpMan.width / 2 + l.width / 2 and jumpMan.x + jumpMan.width / 2 < l.x - l.width / 2 + 10 and jumpMan.y - l.y < jumpMan.height / 2 + l.height / 2 and l.y - jumpMan.y < jumpMan.height / 2 + l.height:
+
+                jumpMan.vx *= -1
+                # find out how much the ostrich's left edge is past the platform's right edge
+                overlap = (jumpMan.x + jumpMan.width / 2) - (l.x - l.width / 2)
+                # "bounce" the ostrich away from the right edge, by that amount.
+                jumpMan.x = (l.x - l.width / 2) - jumpMan.width / 2 - overlap
+
+            elif abs(jumpMan.x - l.x) < jumpMan.width / 2 + l.width / 2:
+                # hitting from below...
+                if jumpMan.vy < 0:  # moving up?
+                    if abs(jumpMan.y - l.y) < jumpMan.height / 2 + l.height / 2 + 1:
+                        jumpMan.vy = abs(jumpMan.vy)
+                elif jumpMan.vy > 0:  # moving down?
+                    if l.y - jumpMan.y < jumpMan.height / 2 + l.height / 2 and l.y > jumpMan.y:
+                        jumpMan.land()
+                        jumpMan.y = l.y - l.height / 2 - jumpMan.height / 2
+
+    elif jumpMan.status == Constants.STATUS_WALKING:
+        # I think I'm walking... is there any ground under my feet?
+        isTouching = False
+        for l in ledgeList:
+            if abs(jumpMan.x - l.x) < jumpMan.width / 2 + l.width / 2 and abs(jumpMan.y - l.y) < jumpMan.height / 2 + l.height / 2 + 1:
+                isTouching = True
+
+        if not isTouching:
+            jumpMan.status = Constants.STATUS_JUMPING
+
+
 
 def checkJumpManPlatformCollisions():
     global jumpMan
@@ -204,21 +279,20 @@ def checkJumpManPlatformCollisions():
             jumpMan.status = Constants.STATUS_JUMPING
 
 
-
 # =====================  readEvents()
 def readEvents():
     """
     checks the list of events and determines whether to respond to one.
     """
     global leftKeyPressed, rightKeyPressed, upKeyPressed, downKeyPressed
-    events = pygame.event.get() #get the list of all events since the last time
-                                #note: this list might be empty.
+    events = pygame.event.get()  # get the list of all events since the last time
+    # note: this list might be empty.
     for evt in events:
-        if evt.type == QUIT: #We got an event! Is it telling us to quit???
+        if evt.type == QUIT:  # We got an event! Is it telling us to quit???
             pygame.quit()
-            raise Exception("User quit the game") # this exception will kick
-                                                  # us out of the main loop at
-                                                  # the bottom of the program.
+            raise Exception("User quit the game")  # this exception will kick
+            # us out of the main loop at
+            # the bottom of the program.
         if evt.type == KEYDOWN:
             if evt.key == K_LEFT:
                 leftKeyPressed = True
@@ -228,7 +302,6 @@ def readEvents():
                 upKeyPressed = True
             if evt.key == K_DOWN:
                 downKeyPressed = True
-
 
         if evt.type == KEYUP:
             if evt.key == K_LEFT:
@@ -240,39 +313,41 @@ def readEvents():
             if evt.key == K_DOWN:
                 downKeyPressed = False
 
-            # You may decide to check other events, like the mouse
-            # or keyboard here.
-#---------------------------------------------------------------------------
+                # You may decide to check other events, like the mouse
+                # or keyboard here.
+
+
+# ---------------------------------------------------------------------------
 # MAIN LOOP   <=======
-#program start with game loop - this is what makes the loop() actually loop.
-#---------------------------------------------------------------------------
-pygame.init() #start up the pygame library
+# program start with game loop - this is what makes the loop() actually loop.
+# ---------------------------------------------------------------------------
+pygame.init()  # start up the pygame library
 
-try: # we're about to try some behavior that might cause the
-     # program to crash. If so, it will now jump to "except," below.
+try:  # we're about to try some behavior that might cause the
+    # program to crash. If so, it will now jump to "except," below.
 
-    preSetup() #do this (mandatory) stuff at the start of the game
-    setup() #do this stuff once at the start of the game. - this is "your" setup.
+    preSetup()  # do this (mandatory) stuff at the start of the game
+    setup()  # do this stuff once at the start of the game. - this is "your" setup.
 
-    fpsClock = pygame.time.Clock() # this will let us pass the deltaT to loop.
+    fpsClock = pygame.time.Clock()  # this will let us pass the deltaT to loop.
 
-    while True: #keep looping forever (or at least until the program crashes)
+    while True:  # keep looping forever (or at least until the program crashes)
 
-        loop(fpsClock.tick(60)/1000.0) #pass the time (in seconds) since the
-                                       # last loop; max speed is 60fps.
+        loop(fpsClock.tick(60) / 1000.0)  # pass the time (in seconds) since the
+        # last loop; max speed is 60fps.
 
-        readEvents() #pay attention to the mouse and keyboard.
+        readEvents()  # pay attention to the mouse and keyboard.
 
 
-except Exception as reason: # if something goes wrong that
-                            # causes the program to stop, let
-                            # us know what's going on.
+except Exception as reason:  # if something goes wrong that
+    # causes the program to stop, let
+    # us know what's going on.
 
-    if reason.message == "User quit the game": #exit gracefully
-                                    #if the user closed the window
+    if reason.message == "User quit the game":  # exit gracefully
+        # if the user closed the window
         print "Game over. User quit the game."
-    else:                           #otherwise, give the programmer
-                                    #some idea what happened.
-        print "Exited program.",reason.message
-        traceback.print_exc() #print what line the program was on
-                              #when it crashed.
+    else:  # otherwise, give the programmer
+        # some idea what happened.
+        print "Exited program.", reason.message
+        traceback.print_exc()  # print what line the program was on
+        # when it crashed.
